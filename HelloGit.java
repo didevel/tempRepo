91e60e569fb97202d987f7c84f316fbf1273f441
Vadim Liubimenko <oth-corp@yandex.ru>
2021-08-27 23:03:55 +0300

    change HelloGit and add SecondGit

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
   public static void main(String[] args){
+    // comment
     System.out.println("it works!");
   }
 }
---
+++ SecondGit.java
@@ -0,0 +1,5 @@
+public class SecondGit{
+  puclic static void main(String[] args){
+    System.out.println("Hello from SecondGit");
+  }
+}
